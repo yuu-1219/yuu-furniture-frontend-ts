c62cdaa28d8c3d21772b36f835d49caa7d4b1ed5
Yuto Kaida <191066813+yuu-1219@users.noreply.github.com>
2025-06-15 16:10:37 +0900

    chore: resolve probrems with importing swipper styles

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 
 declare module '*.css';
+declare module '*.css';
+declare module '*.scss';
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/navigation';
